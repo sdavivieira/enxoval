bcda5b89d2292c3de11fbc8718a0e9c96e310045
Davi Vieira <100722433+sdavivieira@users.noreply.github.com>
2025-01-28 20:02:17 -0300

    melhorando o codigo

--- src/structure/index.js
+++ src/structure/index.js
@@ -13,7 +13,7 @@ import "./style.css";
 export function Structure() {
   const [meta] = useState(10000);
   const [value, setValue] = useState(0);
-  const [rest, setRest] = useState(meta)
+  const [rest, setRest] = useState(meta);
   const [canBuyProducts, setCanBuyProducts] = useState([]);
   const products = [
     { Product: "Geladeira Frost-free", Price: 2500 },
@@ -26,31 +26,28 @@ export function Structure() {
     { Product: "Sofá", Price: 1300 },
   ];
 
-
-
-  function handleCalculated(){
-
+  function handleCalculated() {
     if (value != null && !isNaN(value) && value > 0) {
-    setRest(meta - value)
-   }
-   paintProducts()
-   setValue(0);
+      setRest(meta - value);
+    }
+    paintProducts();
+    setValue(0);
   }
 
-
   function paintProducts() {
-    let currentvalue =  value;
+    let currentvalue = value;
     const productsCanBuy = [];
 
     products.forEach((product) => {
-      if(currentvalue >= product.Price)
+      if (currentvalue >= product.Price) {
         productsCanBuy.push(product.Product);
-      currentvalue -= product.Price
-    })
+        currentvalue -= product.Price;
+      }
+    });
 
-    setCanBuyProducts(productsCanBuy); 
+    setCanBuyProducts(productsCanBuy);
   }
-  
+
   return (
     <div className="page-enxoval">
       <section className="header-Products">
@@ -58,30 +55,50 @@ export function Structure() {
         <h2 className="title-right">Falta: R${rest}</h2>
       </section>
 
-
       <div className="form">
         <text className="form-title">Qual valor que você tem?</text>
         <input
-        className="form-value"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}/>
-        
-        <button className="form-button" onClick={handleCalculated}>Calcular</button>
-        <button className="form-button" onClick={() => {setRest(meta); setCanBuyProducts([])}}>Reiniciar</button>
-      </div>
+          className="form-value"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
 
+        <button className="form-button" onClick={handleCalculated}>
+          Calcular
+        </button>
+        <button
+          className="form-button"
+          onClick={() => {
+            setRest(meta);
+            setCanBuyProducts([]);
+          }}
+        >
+          Reiniciar
+        </button>
+      </div>
 
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell><strong>Produto</strong></TableCell>
-              <TableCell align="right"><strong>Preço (R$)</strong></TableCell>
+              <TableCell>
+                <strong>Produto</strong>
+              </TableCell>
+              <TableCell align="right">
+                <strong>Preço (R$)</strong>
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {products.map((product, index) => (
-              <TableRow key={index}>
+              <TableRow
+                key={index}
+                style={{
+                  backgroundColor: canBuyProducts.includes(product.Product)
+                    ? "pink"
+                    : "inherit",
+                }}
+              >
                 <TableCell>{product.Product}</TableCell>
                 <TableCell align="right">R$: {product.Price}</TableCell>
               </TableRow>
@@ -89,17 +106,6 @@ export function Structure() {
           </TableBody>
         </Table>
       </TableContainer>
-
-      <div className="buy-products">
-      <h3>Produtos que pode comprar:</h3>
-      <ul className="product-list">
-        {canBuyProducts.map((product, index) => (
-          <li key={index} className="product-item">
-            {product}
-          </li>
-        ))}
-      </ul>
-    </div>
     </div>
   );
 }
